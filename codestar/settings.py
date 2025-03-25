eabb093b6f8f57541adf9d387ce85fd88f849855
Nlugner-GitPod <nils@lugner.uk>
2025-03-25 15:38:38 +0000

    updated settings for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^(k3#hy+r7l&5kvtko%jy=nbv*(+anli24y(&jpizx)z+q^)ip'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','heroku.com',]
 
